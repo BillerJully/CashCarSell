504bf7c9817b0392b875bf295a70261079b51b71
Grigory Vasilev <grishavasilev62@gmail.com>
2024-04-20 17:37:47 +0300

    add cors policy

--- main-react/server/index.js
+++ main-react/server/index.js
@@ -4,7 +4,19 @@ const cors = require('cors')
 const index = require('./routes/index')
 const app = express()
 
-app.use(cors())
+const whitelist = ['http://localhost:80'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/', index)
 
